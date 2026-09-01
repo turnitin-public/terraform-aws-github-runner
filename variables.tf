09da301d7fa72b5fcde12bb69e8825adde53fd40
dgignac <dgignac@turnitin.com>
2026-09-01 15:11:28 -0500

    Debug runner disconnect

--- variables.tf
+++ variables.tf
@@ -325,6 +325,7 @@ variable "enable_runner_detailed_monitoring" {
   default     = false
 }
 
+# Debug runner disconnect
 variable "enable_runner_on_demand_failover_for_errors" {
   description = "Enable on-demand failover. For example to fall back to on demand when no spot capacity is available the variable can be set to `InsufficientInstanceCapacity`. When not defined the default behavior is to retry later."
   type        = list(string)
